fd3181106cbf35232c65f26688f9822e621201c9
Daniel Burnside <burnside@Daniels-MacBook-Pro.local>
2019-06-23 07:07:30 -0500

    added a login component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './Login.scss';
+import { Link } from "react-router-dom";
+
+
+export class Login extends Component {
+    render() {
+        return (
+            <form className='login-form'>
+            <h1 className="header-logo">||-----||</h1>
+            <div className="username">
+                <label>Username</label>
+                <input type="text" name="username" />
+            </div>
+            <div className="password">
+                <label>Password</label>
+                <input type="password" name="password" />
+            </div>
+            <div className="buttons">
+                <button className="button1"><Link to={'/register'}>Sign Up</Link></button>
+                <button className="button2"><Link to={'/'}>Sign In</Link></button>  
+            </div>
+            </form>
+
+        )
+    }
+}
+
+export default Login
